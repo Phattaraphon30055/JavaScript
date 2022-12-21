9e88b0ed2d2b1618c7096a38f3ae7e8c23eaf74e
Your Name <you@example.com>
2022-12-21 11:58:55 +0700

    update

--- Lecture2/numconv.js
+++ Lecture2/numconv.js
@@ -3,29 +3,29 @@ let fol1 = '1.50';
 
 console.log("*******Converting string to integers********")
 //Converting string to integers
-console.log(paeseInt('100'));
-console.log(paeseInt('num1'));
-console.log(paeseInt('ABC'));
-console.log(paeseInt('0xF')); //Hexadecimal number
+console.log(parseInt('100'));
+console.log(parseInt('num1'));
+console.log(parseInt('ABC'));
+console.log(parseInt('0xF')); //Hexadecimal number
 
 console.log("*******Converting string to float********")
 //Converting string to integers
-console.log(paesefloat('1.25abc'));
-console.log(paesefloat('flo1'));
-console.log(paesefloat('ABC'));
+console.log (parseFloat('1.25abc'));
+console.log(parseFloat('flo1'));
+console.log(parseFloat('ABC'));
 
 console.log("*******Moer Converting Examples********")
 //Moer Converting Examples
 //Number after speial characters are ignored
-console.log(paeseInt('1.5'));
-console.log(paeseInt('1+1'));
+console.log(parseInt('1.5'));
+console.log(parseInt('1+1'));
 
 //Using Template Literals
-console.log(paeseInt(`${1+1}`));
+console.log(parseInt(`${1+1}`));
 
 console.log("*******Converting numbers to string  ********")
 //Converting numbers to string
 console.log (num1.toString());
-console.log (flo1.toString());
+//console.log (flo1.toString());
 console.log ((100).toString());
 
