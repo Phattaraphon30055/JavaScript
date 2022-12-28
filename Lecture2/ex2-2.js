eb4a155602d9a6873ad0f7b3cc4a506a1ab1a5ab
Your Name <you@example.com>
2022-12-28 10:38:43 +0700

    update

---
+++ Lecture2/ex2-2.js
@@ -0,0 +1,7 @@
+function evallEquation(word){
+    return eval(word);
+}
+    console.log(evallEquation("1+1"));
+    console.log(evallEquation("7*4-2"));
+    console.log(evallEquation("1+1+1+1+1"));
+    
