d12d4b8956955ca46cde2bf1958a7d06e26868c0
Your Name <you@example.com>
2022-12-28 09:56:24 +0700

    update

--- Lecture3/implecit-false.js
+++
@@ -1,8 +0,0 @@
-const name = '';
-
-if (name) {
-    console.log ('We have a name !');
-
-}else{
-    console.log('No name provided');
-}
--- Lecture3/undefined.js
+++
@@ -1,9 +0,0 @@
-function test(t) {
-    if (t === undefined){
-        return "Undefined Value!"
-    }
-    return t;
-}
-
-let x ;
-console.log (test(x));
