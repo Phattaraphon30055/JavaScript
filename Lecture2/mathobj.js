5e7306ead2cf293675e523efca8aa64f92cae4c9
Your Name <you@example.com>
2022-12-28 09:20:25 +0700

    update

--- Lecture2/mathobj.js
+++ Lecture2/mathobj.js
@@ -4,7 +4,6 @@ console.log('starting timer...');
 
 setTimeout(() => {
     const millis =Date.now() - start;
-
-    console.log(seconds elapsed = ${Math.floor(millis / 1000)});
+    console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
 
 },2000);
